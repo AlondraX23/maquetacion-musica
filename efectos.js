f3a4ffe7a82d6609f58d9ebd2128cf3225ba6996
AlondraX23 <axdamian.23@gmail.com>
2023-08-08 06:32:16 -0600

    Arreglo CRUD + read

--- efectos.js
+++ efectos.js
@@ -13,3 +13,26 @@ function irBandas(url) {
 function irConciertos(url) {
     window.location.href = url;
 }
+
+var btnTablaAlbums = document.getElementById('btnTablaAlbums');
+
+var tablaAlbums = document.getElementById('tablaAlbums');
+var formAlbums = document.getElementById('formAlbums');
+
+btnTablaAlbums.addEventListener('click', function () {
+    if (formAlbums.style.display === 'none') {
+        tablaAlbums.style.display = 'block';
+        formAlbums.style.display = 'none';
+    } else {
+        tablaAlbums.style.display = 'none';
+        formAlbums.style.display = 'block';
+    }
+})
+
+if (tablaAlbums.style.display === 'none') {
+    tablaAlbums.style.display = 'block';
+    formAlbums.style.display = 'none';
+} else {
+    tablaAlbums.style.display = 'none';
+    formAlbums.style.display = 'block';
+}
