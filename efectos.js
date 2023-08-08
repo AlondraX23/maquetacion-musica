12b74523bcb89a40a743fe3da7089e001579a356
AlondraX23 <axdamian.23@gmail.com>
2023-08-08 01:45:51 -0600

    Base CRUD

---
+++ efectos.js
@@ -0,0 +1,15 @@
+function irAlbums(url) {
+    window.location.href = url;
+}
+
+function irGeneros(url) {
+    window.location.href = url;
+}
+
+function irBandas(url) {
+    window.location.href = url;
+}
+
+function irConciertos(url) {
+    window.location.href = url;
+}
